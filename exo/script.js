0e45acf8e2fc0403ddab8be265ab3d2ba72cbdf3
ycronel <caroline-lenorcy@orange.fr>
2023-02-13 16:36:32 +0100

    création de deux tableaux remplis de manière aléatoire

--- exo/script.js
+++ exo/script.js
@@ -1,1 +1,17 @@
+// Créer 2 tableaux avec 20 chiffres ou nombres
+let firstArray = [];
+let secondArray = [];
+
+for (let i = 0; i < 20; i++) {
+    firstArray[i] = Math.floor(Math.random() * 100);
+    secondArray[i] = Math.floor(Math.random() * 100);
+    
+}
+console.table(firstArray);
+console.table(secondArray);
+// commit !
+// Créer une fonction pour ordonner ses tableaux 
+// commit !
+// Afficher le nombre médian
+// commit !
 
